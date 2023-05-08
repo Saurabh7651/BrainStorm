ff207e4b4b817405659ef7fc26c566a950752d2e
Saurabh7651 <Saurabhp.8382@gmail.com>
2023-05-09 01:57:10 +0530

    first commit

---
+++ dictionary/jspart.js
@@ -0,0 +1,7 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result=document.getElementsByClassName("container")
+const btn=document.getElementById("button")
+// btn.addEventListener("click",myfunction);
+ function myfunction(){
+    document.getElementsByClassName("container").innerHTML=d`"hii"`;
+}
